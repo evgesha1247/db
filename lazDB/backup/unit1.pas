afd7bed4c7e4806368fedcf2a2ae23321c635b46
Evgesha <70853142+evgesha1247@users.noreply.github.com>
2022-06-13 21:33:18 +0300

    Add files via upload

---
+++ lazDB/backup/unit1.pas
@@ -0,0 +1,210 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, MSSQLConn, SQLDB, DB, SQLite3Conn, Forms, Controls,
+  Graphics, Dialogs, StdCtrls, DBGrids, DBCtrls, ExtCtrls, ComCtrls;
+
+type
+  { TForm1 }
+  TForm1 = class(TForm)
+    ButtonBooks: TButton;
+    ButtonAdd: TButton;
+    ButtonDel: TButton; ButtonWriters: TButton;
+    ButtonEdit: TButton;
+
+    DBGrid1: TDBGrid;
+    DataS: TDataSource;
+    DBLookupComboBox2: TDBLookupComboBox;
+    DBLookupComboBox3: TDBLookupComboBox;
+    DBLookupComboBox4: TDBLookupComboBox;
+    DS2: TDataSource;
+    DS3: TDataSource;
+    DS4: TDataSource;
+    EditKol: TEdit;
+    EditSearch: TEdit;
+    Image1: TImage;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    SQLQ2: TSQLQuery;
+    SQLQ3: TSQLQuery;
+    SQLQ4: TSQLQuery;
+
+    SQLQuery: TSQLQuery;
+    SQLQueryAdd: TSQLQuery;
+    SQLQueryZapros: TSQLQuery;
+    SQLT: TSQLTransaction;
+    SQLC: TSQLite3Connection;
+
+
+    procedure ButtonAddClick(Sender: TObject);
+    procedure ButtonDelClick(Sender: TObject);
+    procedure ButtonWritersClick(Sender: TObject);
+    procedure ButtonEditClick(Sender: TObject);
+
+    procedure ButtonBooksClick(Sender: TObject);
+    procedure DBGrid1CellClick(Column: TColumn);
+    procedure DBGrid1TitleClick(Column: TColumn);
+
+
+    procedure EditSearchChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure sqlDBError(const errorStr: string) ;
+  private
+
+  public
+    procedure ActiveQueryAndClear;
+  end;
+
+var
+   Form1 : TForm1;
+   Id : integer;
+   sql : string ;
+implementation
+   uses Unit3 , Unit4;
+{$R *.lfm} { TForm1 }
+
+ 
+procedure TForm1.FormCreate(Sender: TObject);
+  begin
+    sql := ' select products.idWriter,publishers.idProduct,publishers.idBookBinding ,publishers.idPublisHers ,  publishers.id, writers.writers_name,writers.patronymic,writers.surname,products.products_name, publishingHouse.publisherName ,bookBinding.bookBinding_name ,publishers.circulation from publishers left join products on publishers.idProduct = products.id left join bookBinding on publishers.idBookBinding = bookBinding.id left join writers on  products.idWriter = writers.id  left join publishingHouse on publishers.idPublisHers = publishingHouse.id ';
+    SQLC.DatabaseName := '/home/evg/Документы/практика/lazDB/db/writes';
+    SQLC.CharSet := 'UTF8';
+    SQLC.Transaction := SQLT;
+  end;
+
+procedure TForm1.FormShow(Sender: TObject);
+  begin
+     try
+       SQLC.Open;
+       SQLC.Connected := True;
+       SQLQuery.Active := True;
+       SQLT.Active := True;
+     except sqlDBError('база не подключена')
+     end;
+     ActiveQueryAndClear;
+  end;
+
+
+
+procedure TForm1.DBGrid1CellClick(Column: TColumn);
+  begin
+    id := SQLQuery.FieldByName('id').AsInteger;
+    DBLookupComboBox2.KeyValue := SQLQuery.FieldByName('idProduct').AsInteger;
+    DBLookupComboBox3.KeyValue := SQLQuery.FieldByName('idPublisHers').AsInteger;
+    DBLookupComboBox4.KeyValue := SQLQuery.FieldByName('idBookBinding').AsInteger;
+    EditKol.Text :=  SQLQuery.FieldByName('circulation').AsString;
+  end;
+
+procedure TForm1.DBGrid1TitleClick(Column: TColumn);
+begin
+    SQLQuery.Close;
+    SQLQuery.SQL.Clear;
+    SQLQuery.SQL.Add(SQL);
+     if Column.FieldName = 'id' then begin
+       SQLQuery.SQL.Add('ORDER BY publishers.id');
+     end
+     else begin
+        SQLQuery.SQL.Add('ORDER BY ' + Column.FieldName);
+     end;
+
+end;
+
+procedure TForm1.EditSearchChange(Sender: TObject);
+begin
+   if EditSearch.text = '' then begin
+      SQLQuery.Active := false;
+      SQLQuery.SQL.Clear;
+      SQLQuery.SQL.Add(sql);
+      SQLQuery.Active := True;
+   end
+   else begin
+      SQLQuery.Active := false;
+      SQLQuery.SQL.Add(sql);
+      SQLQuery.SQL.Add('where  writers.surname like ''%' + EditSearch.text + '%'';');
+      SQLQuery.Active := True;
+    end;
+end;
+
+
+procedure TForm1.ButtonAddClick(Sender: TObject);
+begin
+    SQLQuery.Active:= False;
+    SQLQueryAdd.Close;
+    SQLQueryAdd.Clear;
+    SQLQueryAdd.SQL.Text := 'insert into publishers(id, idPublisHers, idProduct, idBookBinding, circulation) values(:id, :idPublisHers, :idProduct, :idBookBinding, :circulation); ';
+    SQLQueryAdd.ParamByName('idProduct').AsInteger := DBLookupComboBox2.KeyValue;
+    SQLQueryAdd.ParamByName('idPublisHers').AsInteger := DBLookupComboBox3.KeyValue;
+    SQLQueryAdd.ParamByName('idBookBinding').AsInteger := DBLookupComboBox4.KeyValue;
+    SQLQueryAdd.ParamByName('circulation').AsString := EditKol.Text ;
+    SQLQueryAdd.ExecSQL;
+    Unit1.Form1.SQLT.Commit;
+    ActiveQueryAndClear;
+end;
+
+procedure TForm1.ButtonEditClick(Sender: TObject);
+begin
+    SQLQuery.Active:= False;
+    SQLQueryZapros.Close;
+    SQLQueryZapros.SQL.Text := 'update publishers set idProduct = :idProduct, idPublisHers = :idPublisHers, idBookBinding = :idBookBinding, circulation = :circulation where id = :id ; ';
+    SQLQueryZapros.ParamByName('id').AsInteger :=  id;
+    SQLQueryZapros.ParamByName('idProduct').AsInteger := DBLookupComboBox2.KeyValue;
+    SQLQueryZapros.ParamByName('idPublisHers').AsInteger := DBLookupComboBox3.KeyValue;
+    SQLQueryZapros.ParamByName('idBookBinding').AsInteger := DBLookupComboBox4.KeyValue;
+    SQLQueryZapros.ParamByName('circulation').AsString := EditKol.Text ;
+    SQLQueryZapros.ExecSQL;
+    SQLT.Commit;
+    ActiveQueryAndClear;
+end;
+
+procedure TForm1.ButtonDelClick(Sender: TObject);
+  begin
+    SQLQuery.Active:= False;
+    SQLQueryZapros.Close;
+    SQLQueryZapros.SQL.Text := 'delete from publishers where id=:id ';
+    SQLQueryZapros.ParamByName('id').AsInteger := id;
+    SQLQueryZapros.ExecSQL;
+    SQLT.Commit;
+    ActiveQueryAndClear;
+  end;
+
+procedure TForm1.ButtonWritersClick(Sender: TObject);
+   begin
+     Unit4.Form6.Show;
+   end;
+
+procedure TForm1.ButtonBooksClick(Sender: TObject);
+   begin
+     Unit3.FormBook.Show;
+   end;
+
+procedure TForm1.sqlDBError(const errorStr: string);
+  begin
+    ShowMessage('ERROR: ' + errorStr);
+  end;
+
+procedure TForm1.ActiveQueryAndClear;
+  begin
+     SQLQuery.Active:= True;
+     SQLQ2.Active:= True;
+     SQLQ3.Active:= True;
+     SQLQ4.Active:= True;
+
+     DBLookupComboBox2.ListFieldIndex :=  0 ;
+     DBLookupComboBox3.ListFieldIndex :=  0 ;
+     DBLookupComboBox4.ListFieldIndex :=  0 ;
+     EditKol.text := '';
+  end;
+
+
+
+
+end.
+
---
+++ lazDB/формы/unit3.pas
@@ -0,0 +1,145 @@
+unit Unit3;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, SQLDB, DB, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  DBCtrls, DBGrids, ExtCtrls;
+
+type
+
+  { TFormBook }
+
+  TFormBook = class(TForm)
+    Button1: TButton; Button2: TButton;
+    ButtonNext: TButton;
+    ButtonEdit: TButton;
+
+    DataSource1: TDataSource;
+    DataSource2: TDataSource;
+    DataSourceDel: TDataSource;
+
+    DBGrid1: TDBGrid;
+    DBLookupComboBox1: TDBLookupComboBox;
+    DBLookupComboBox2: TDBLookupComboBox;
+
+    Edit1: TEdit;    Edit2: TEdit;
+    Image1: TImage;
+    Label1: TLabel;  Label2: TLabel;
+    Label3: TLabel;  Label4: TLabel;
+    Label5: TLabel;
+
+    SQLQuery1:   TSQLQuery;
+    SQLQuery2:   TSQLQuery;
+    SQLQuery:    TSQLQuery;
+    SQLQueryZapros: TSQLQuery;
+    SQLQueryDel: TSQLQuery;
+    SQLQueryAdd: TSQLQuery;
+
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure ButtonNextClick(Sender: TObject);
+    procedure ButtonEditClick(Sender: TObject);
+    procedure DBGrid1CellClick(Column: TColumn);
+    procedure FormShow(Sender: TObject);
+
+  private
+
+  public
+     procedure ActiveQueryAndClear;
+  end;
+
+var
+  FormBook: TFormBook;
+
+  idProducts : integer;
+implementation
+   uses Unit1;
+{$R *.lfm} { TFormBook }
+
+procedure TFormBook.FormShow(Sender: TObject);
+  begin
+    ActiveQueryAndClear;
+  end;
+
+procedure TFormBook.Button1Click(Sender: TObject);
+  begin
+    SQLQuery.Active:= False;
+    SQLQueryAdd.Close;
+    SQLQueryAdd.Clear;
+    SQLQueryAdd.SQL.Text := 'insert into products(id,products_name, idWriter , idGenre , remark) values(:id, :products_name,  :idWriter , :idGenre , :remark); ';
+    SQLQueryAdd.ParamByName('products_name').AsString := Edit1.Text;
+    SQLQueryAdd.ParamByName('idWriter').AsInteger := DBLookupComboBox1.KeyValue;
+    SQLQueryAdd.ParamByName('idGenre').AsInteger := DBLookupComboBox2.KeyValue;
+    SQLQueryAdd.ParamByName('remark').AsString := Edit2.Text ;
+    SQLQueryAdd.ExecSQL;
+    Unit1.Form1.SQLT.Commit;
+    ActiveQueryAndClear;
+  end;
+
+
+procedure TFormBook.Button2Click(Sender: TObject);
+  begin
+    SQLQuery.Active:= False;
+    SQLQueryDel.Close;
+    SQLQueryDel.SQL.Text := 'delete from products where id=:id ';
+    SQLQueryDel.ParamByName('id').AsInteger := idProducts;
+    SQLQueryDel.ExecSQL;
+    Unit1.Form1.SQLT.Commit;
+    ActiveQueryAndClear;
+  end;
+
+procedure TFormBook.ButtonEditClick(Sender: TObject);
+begin
+    SQLQuery.Active:= False;
+    SQLQueryZapros.Close;
+    SQLQueryZapros.sql.clear;
+
+    SQLQueryZapros.SQL.Text := 'update products set products_name = :products_name, idWriter = :idWriter, ';
+    SQLQueryZapros.SQL.Add('idGenre = :idGenre, remark = :remark ');
+    SQLQueryZapros.SQL.Add('where id = :idProducts ');
+
+    SQLQueryZapros.ParamByName('idProducts').AsInteger :=  idProducts;
+    SQLQueryZapros.ParamByName('products_name').AsString :=  Edit1.Text;
+    SQLQueryZapros.ParamByName('idWriter').AsInteger := DBLookupComboBox1.KeyValue;
+    SQLQueryZapros.ParamByName('idGenre').AsInteger := DBLookupComboBox2.KeyValue;
+    SQLQueryZapros.ParamByName('remark').AsString := Edit2.Text;
+
+    SQLQueryZapros.ExecSQL;
+    Unit1.Form1.SQLT.Commit;
+    ActiveQueryAndClear;
+  end;
+
+procedure TFormBook.DBGrid1CellClick(Column: TColumn);
+  begin
+     idProducts := SQLQuery.FieldByName('id').AsInteger;
+     Edit1.text := SQLQuery.FieldByName('products_name').AsString;
+     Edit2.text := SQLQuery.FieldByName('remark').AsString;
+     DBLookupComboBox1.KeyValue := SQLQuery.FieldByName('idWriter').AsInteger;
+     DBLookupComboBox2.KeyValue := SQLQuery.FieldByName('idGenre').AsInteger;
+  end;
+
+procedure TFormBook.ActiveQueryAndClear;
+  begin
+     Unit1.Form1.SQLQuery.Active := True;
+     SQLQuery.Active:= True;
+     SQLQuery1.Active:= True;
+     SQLQuery2.Active:= True;
+     DBLookupComboBox1.ListFieldIndex :=  0 ;
+     DBLookupComboBox2.ListFieldIndex :=  0 ;
+     Edit1.text := '';
+     Edit2.text  := '';
+  end;
+
+
+
+procedure TFormBook.ButtonNextClick(Sender: TObject);
+begin
+   Unit1.Form1.ActiveQueryAndClear;
+   Unit1.Form1.Show;
+end;
+
+end.
+
